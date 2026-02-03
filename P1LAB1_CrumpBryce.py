6334803a2c34e6f72f33ec20a3fc297efbe1d4b7
Bryce-Crump <crumpb6082@student.faytechcc.edu>
2026-02-03 18:36:43 -0500

    Add files via upload

---
+++ P1LAB1_CrumpBryce.py
@@ -0,0 +1,11 @@
+# Bryce Crump
+# 2/3/2026
+# P1LAB1
+# First & Last name python program
+#input variables
+first_name = input("Type your first name, then hit ENTER: ")
+last_name = input("Type your last name, then hit ENTER: ")
+#data collection variable
+data = f"{first_name} {last_name}"
+#print commands
+print(f"Hello, {data}! Welcome to CTI 110, a super fun class!")    
